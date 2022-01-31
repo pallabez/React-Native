751cf4a7b4270b40f3345accbb9c48a56c9ab98b
pallabez <pallabsonowal999@gmail.com>
2022-02-01 02:49:14 +0530

    finished shopping cart app

--- Shopping Cart App/app.tsx
+++ Shopping Cart App/app.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
+import React, { useState, createContext, useContext } from 'react'
 import { FlatList, Text, View, StatusBar, StyleSheet, SafeAreaView, Alert, Button, Image , Dimensions, TouchableOpacity} from 'react-native';
 import { NavigationContainer, NavigationProp, ParamListBase } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import { useState } from 'react';
 
 const DATA = [
   {
@@ -23,21 +21,33 @@ const DATA = [
   },
 ];
 
-const ItemButton = () => {
+const ItemButton = ({ price }) => {
+  const [count, setCount] = useState(0);
+  const {total, setTotal, numberOfItems, setNumberOfItems} = useContext(ItemContext);
+
   return (
     <View style={styles.itemButtonContainer}>
       <TouchableOpacity
         style={[styles.itemButton, {borderTopLeftRadius: 6, borderBottomLeftRadius: 6}]}
-        onPress={() => {}}
+        disabled={count == 0}
+        onPress={() => {
+          setCount(count - 1);
+          setTotal(Math.round((total - price)*100)/100);
+          setNumberOfItems(numberOfItems - 1);
+        }}
       >
         <Text style={styles.buttonText}>-</Text>
       </TouchableOpacity>
       <View style={styles.itemCount}> 
-        <Text style={styles.buttonText}>3</Text>
+        <Text style={styles.buttonText}>{count}</Text>
       </View>
       <TouchableOpacity
         style={[styles.itemButton, {borderTopRightRadius: 6, borderBottomRightRadius: 6}]}
-        onPress={() => {}}
+        onPress={() => {
+          setCount(count + 1);
+          setTotal(Math.round((total + price)*100)/100);
+          setNumberOfItems(numberOfItems + 1);
+        }}
       >
         <Text style={styles.buttonText}>+</Text>
       </TouchableOpacity>
@@ -59,17 +69,20 @@ const Item: React.FC = ({ title, id, price}) => {
       <View style={styles.itemDetailContainer}>
         <Text style={styles.title}>{title}</Text>
         <Text style={{fontSize: 20,fontWeight: "bold"}}>$ {price} </Text>
-        <ItemButton />
+        <ItemButton price={price}/>
       </View>
     </View>
   );
 }
 
+const ItemContext = createContext();
+
 const HomeScreen: React.FC<{}> = ({ navigation }) => {
   const [data, setData] = useState(DATA);
+  const {total, numberOfItems} = useContext(ItemContext);
 
   const renderItem: React.FC<{item : {id: String, title: String}}> = ({item}) => (
-    <Item 
+    <Item
       title={item.title}
       price={item.price}
       id={item.id}
@@ -85,39 +98,64 @@ const HomeScreen: React.FC<{}> = ({ navigation }) => {
       />
       <TouchableOpacity
         style={styles.checkoutButton}
-        onPress={() => navigation.navigate('Checkout')}
+        onPress={() => navigation.navigate(`Checkout`)}
       >
-        <Text style={{ fontSize: 20, textAlign: 'center'}}>Checkout</Text>
+        <Text style={{ fontSize: 20, textAlign: 'center'}}>Checkout ({numberOfItems})</Text>
       </TouchableOpacity>
     </SafeAreaView>
   );
 }
 
-const CheckoutScreen = () => {
+const SuffixTest = ({addStyle, children}) => {
   return (
-    <Text>
-    </Text>
+    <Text style={[styles.suffixTest, addStyle]}>{children}</Text>
+  )
+}
+
+const CheckoutScreen = ({navigation}) => {
+  const {total, numberOfItems} = useContext(ItemContext);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={{flexDirection: "row", margin: 25}}>
+        <Text style={{fontSize: 25}}>Total </Text>
+        <SuffixTest addStyle={{fontSize:20, fontWeight:'bold'}} children='$'/>
+        <Text style={{fontSize: 25, fontWeight:'bold'}}> {total}</Text>
+      </View>
+      <View style={styles.checkoutContainer}>
+        <Text style={{fontSize: 25}}>Total items: {numberOfItems}</Text>
+      </View>
+      <TouchableOpacity
+        style={styles.checkoutButton}
+        onPress={() => Alert.alert('Itne paise mein itna hi milega')}
+      >
+        <Text style={{ fontSize: 20, textAlign: 'center'}}>Pay ${total}</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 };
-
 
 const Stack = createNativeStackNavigator();
 
 const App: React.FC<{}> = () => {
+  const [total, setTotal] = useState(0);
+  const [numberOfItems, setNumberOfItems] = useState(0);
+
   return (
+    <ItemContext.Provider value={{total, setTotal, numberOfItems, setNumberOfItems}}>
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Cart' }} />
         <Stack.Screen name="Checkout" component={CheckoutScreen} />
       </Stack.Navigator>
     </NavigationContainer>
+    </ItemContext.Provider>
   );  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
   },
   title: {
     fontSize: 18,
@@ -128,10 +166,12 @@ const styles = StyleSheet.create({
     textAlignVertical: 'center',
     margin: 4,
   },
+  suffixTest: {
+    fontSize: 25,
+  },
   itemContainer: { 
     marginBottom: 20,
     padding: 20,
-    flex: 1,
     flexDirection: "row",
     justifyContent: "flex-start",
     borderBottomWidth: 0.5,
@@ -158,10 +198,18 @@ const styles = StyleSheet.create({
   },
   checkoutButton: {
     marginBottom: 8,
+    marginTop: 25,
     marginHorizontal: 4,
     borderRadius: 20,
     backgroundColor: '#ffd814',
     paddingVertical: 12,
+  }, 
+  checkoutContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 2,
+    marginHorizontal: 25,
   }
 });
 
